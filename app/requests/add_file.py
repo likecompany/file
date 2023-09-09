0b1194f1a23c6b5891d9a4e8c3c08d62408b61f3
toymaj <sermed512@gmail.com>
2023-09-09 18:00:17 +0300

    fix errors

--- app/requests/add_file.py
+++ app/requests/add_file.py
@@ -9,5 +9,5 @@ from schema import ApplicationSchema
 
 class AddFileRequest(ApplicationSchema):
     access_token: str
-    file_name: str = Field(..., max_length=256)
+    file_name: Optional[str] = Field(None, max_length=256)
     mime_type: Optional[str] = Field(None, max_length=128)
