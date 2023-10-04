ed748cccf9b24a872da3ce3abe84989602cfe544
toymaj <sermed512@gmail.com>
2023-10-04 23:38:00 +0300

    Remove scripts and license

--- app/schema.py
+++ app/schema.py
@@ -4,18 +4,18 @@ from typing import Any, Generic, Optional, TypeVar
 
 from pydantic import BaseModel, ConfigDict
 
-AuthType = TypeVar("AuthType", bound=Any)
+FileType = TypeVar("FileType", bound=Any)
 
 
 class ApplicationSchema(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
 
-class ApplicationResponse(BaseModel, Generic[AuthType]):
+class ApplicationResponse(BaseModel, Generic[FileType]):
     model_config = ConfigDict(populate_by_name=True)
 
     ok: bool
-    result: Optional[AuthType] = None
+    result: Optional[FileType] = None
     detail: Optional[str] = None
     error: Optional[str] = None
     error_code: Optional[int] = None
--- scripts/mypy.sh
+++
@@ -1,2 +0,0 @@
-python3 -m pip install -U pip
-python3 -m pip install types-passlib types-pytz
